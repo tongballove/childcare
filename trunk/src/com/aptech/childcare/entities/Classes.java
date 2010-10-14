ce4e264d695871d8bb97b906a2bbf22e40ee7abd
chieuhue <chieuhue@3315ff17-3998-a2f6-7a5f-aaa583b84b44>
2010-10-14 16:56:36 +0000

    Update Coding...

---
+++ trunk/src/com/aptech/childcare/entities/Classes.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.aptech.childcare.entities;
+
+/**
+ *
+ * @author Dang Van Thanh
+ */
+public class Classes {
+
+    private Integer classid;
+    private String classname;
+    private String notes;
+    private AgeGroup ageGroup;
+
+    public Classes() {
+    }
+
+    public Classes(Integer classid) {
+        this.classid = classid;
+    }
+
+    public Classes(Integer classid, String classname) {
+        this.classid = classid;
+        this.classname = classname;
+    }
+
+    public Integer getClassid() {
+        return classid;
+    }
+
+    public void setClassid(Integer classid) {
+        this.classid = classid;
+    }
+
+    public String getClassname() {
+        return classname;
+    }
+
+    public void setClassname(String classname) {
+        this.classname = classname;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    public AgeGroup getAgeGroup() {
+        return ageGroup;
+    }
+
+    public void setAgeGroup(AgeGroup ageGroup) {
+        this.ageGroup = ageGroup;
+    }
+}
